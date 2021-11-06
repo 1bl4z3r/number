e46ce1ccd905cd147d8621cfb7774f04e2b14624
1bl4z3r <1bl4z3r@users.noreply.github.com>
2021-11-06 21:01:16 +0530

    Tidying things up. Check the new update in Readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Frustating Number Game
 because why not
+
+## Update 06 Nov 2021
+- Added limited lives as suggested by [Samir Dubey](https://github.com/heydubey)
+- Made the page responsive and used grid layout
--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var output = document.getElementById("value");
 var submit = document.getElementById("submit");
 var score = document.getElementById("score");
 var pgoal = document.getElementById("pgoal");
+var live = document.getElementById("live");
+live.innerHTML = 5;
+score.innerHTML = 0;
 var maxim = 999999999999;
+var deadon = 0;
 var goal = resetgoal();
 output.innerHTML = slider.value;
 
@@ -12,26 +16,48 @@ slider.oninput = function() {
 }
 slider.onpointerdown = function(){
     output.innerHTML = this.value;
-    document.getElementById("pval").style.visibility = "hidden";
+    dhide();
+}
+submit.onclick = function(){
+    if(Number(live.innerHTML) != 0){
+        
+        var ps = Math.round((maxim - ((output.innerHTML > goal) ? (output.innerHTML - goal) : (goal - output.innerHTML)))/100000000);
+        score.innerHTML = Math.round((Number(score.innerHTML) + ps)/(6 - live.innerHTML));
+
+        if(ps == 10000){
+            document.getElementById('deadon').style.display = "block";
+            deadon +=1;
+        }else if(ps >= 6000 && ps < 10000){
+            document.getElementById('soclose').style.display = "block";
+        }else if(ps < 6000){
+            document.getElementById('sofar').style.display = "block";
+        }
+        live.innerHTML -= 1;
+        document.getElementById("pd").style.visibility = "visible";
+        pgoal.innerHTML = "Your Goal was: " + goal;
+        goal = resetgoal();
+    }else if(Number(live.innerHTML) == 0) {
+        slider.disabled = true;
+        document.getElementById("submit").disabled = true;
+        dhide();
+        sc();    
+    }
+};
+function dhide(){
+    document.getElementById("pd").style.visibility = "hidden";
     document.getElementById("deadon").style.display = 'none';
     document.getElementById("soclose").style.display = 'none';
     document.getElementById("sofar").style.display = 'none';
 }
-submit.onclick = function(){
-    var ps = Math.round((maxim - ((output.innerHTML > goal) ? (output.innerHTML - goal) : (goal - output.innerHTML)))/100000000);
-    score.innerHTML = ps;
-
-    if(ps == 10000){
-        document.getElementById('deadon').style.display = "block";
-    }else if(ps >= 6000 && ps < 10000){
-        document.getElementById('soclose').style.display = "block";
-    }else if(ps < 6000){
-        document.getElementById('sofar').style.display = "block";
+function sc(){
+    document.getElementById("pd").style.visibility = "visible";
+    if(deadon != 0){
+        pgoal.innerHTML = "You have exhausted your chances. You scored "+ score.innerHTML + " with "+deadon+" deadons";
+    }else{
+        pgoal.innerHTML = "You have exhausted your chances. You scored "+ score.innerHTML;
     }
-    document.getElementById("pval").style.visibility = "visible";
-    pgoal.innerHTML = goal;
-    goal = resetgoal();
-};
+    score.innerHTML = 0;
+}
 function resetgoal(){
-    return(Math.floor(Math.random() * (maxim - 111111111111 + 1) + 111111111111));
+    return (Math.floor(Math.random() * (maxim - 111111111111 + 1) + 111111111111));
 }
